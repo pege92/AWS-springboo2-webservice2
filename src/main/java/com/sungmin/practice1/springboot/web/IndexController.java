81f753ec59b0259e7e27a0c05883187e5a81d873
sungmin <pege92@gmail.com>
2021-04-11 21:05:11 +0900

    naver까지 완료

--- src/main/java/com/sungmin/practice1/springboot/web/IndexController.java
+++ src/main/java/com/sungmin/practice1/springboot/web/IndexController.java
@@ -1,5 +1,9 @@
 package com.sungmin.practice1.springboot.web;
 
+import com.sungmin.practice1.springboot.config.auth.LoginUser;
+import com.sungmin.practice1.springboot.config.auth.dto.SessionUser;
+
+import com.sungmin.practice1.springboot.domain.user.User;
 import com.sungmin.practice1.springboot.service.PostsService;
 import com.sungmin.practice1.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,10 +22,16 @@ public class IndexController {
     }*/
 
     private final PostsService postsService;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+
+        }
         return "index";
     }
 
